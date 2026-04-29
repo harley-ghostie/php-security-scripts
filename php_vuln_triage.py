a42a65eb4832ed385734a2c8f95c964dd1eb4c69
harley-ghostie <harleyvpn99@proton.me>
2026-04-29 15:34:28 -0300

    Update php_vuln_triage.py
    Triagem inicial para validar indícios de LFI, deserialização insegura e phpinfo() exposto

--- php_vuln_triage.py
+++ php_vuln_triage.py
@@ -1,7 +1,7 @@
 import requests
 import urllib.parse
 
-TARGET = "http://www.alvo.com.br/caminho"  # <--- Substituir pelo alvo real
+TARGET = "http://www.alvo.com.br/caminho/"  # <--- Substituir pelo alvo real
 
 def test_lfi():
     print("[*] Testando LFI em parâmetros comuns...")
